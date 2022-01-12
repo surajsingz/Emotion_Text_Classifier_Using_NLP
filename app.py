4037de1f4c450c57c72850699b116954bf072af9
Suraj Singh <surajbksingh157@gmail.com>
2022-01-12 17:49:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from multiapp import Multiapp
+from pages import home, text, speech, emotionClassifier
+
+app = Multiapp()
+
+app.add_app("Home", home.app)
+app.add_app("Emotion Text Classifier", emotionClassifier.app)
+#app.add_app("Speech to text", speech.app)
+#app.add_app("Translation (In progress)", text.app)
+
+
+
+app.run()
